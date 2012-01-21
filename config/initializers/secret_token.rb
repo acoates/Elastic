65b4f209a6d73696c861e45d02bf36d512f460bf
Andrew Coates <acoates@infotech.com>
2012-01-21 12:08:27 -0500

    Created app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Elastic::Application.config.secret_token = 'f3b4d3f6abc130d33ac0ef9cc247c9f9865f531409e031afcc8370437938df38d84784a522dd48faea3d5d6d313a55e52dbe66b00b9b2fefd937bb21183f9f7e'
