13be792ea0d674313be2eba8ef1cb736185a3321
Andrew Coates <acoates@infotech.com>
2012-08-09 22:15:59 -0400

    added active_attr and contact form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,16 @@
 class MessagesController < ApplicationController
 
-def create
-  @message = Message.new(params[:message])
-  if @message.valid?
-    # TODO send message here
-    flash[:notice] = "Message sent! Thank you for contacting us."
-    redirect_to root_url
-  else
-    render :action => 'new'
+  def new
+    @message = Message.new
   end
+
+  def create
+    @message = Message.new(params[:message])
+    if @message.valid?
+      # TODO send message here
+      flash[:notice] = "Message sent! Thank you for contacting us."
+      redirect_to root_url
+    else
+      render :action => 'new'
+    end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -7,7 +7,7 @@ class Message
   
   validates_presence_of :name
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
-  validates_length_of :content, :maximum => 500
+  validates_length_of :content, :maximum => 1500
   
   def initialize(attributes = {})
     attributes.each do |name, value|
