ec3f9fc7293020620112e9605d83055a70218d26
Andrew Coates <acoates@infotech.com>
2012-08-12 16:44:03 -0400

    Added photos to team page

--- app/mailers/elastic_mailer.rb
+++ app/mailers/elastic_mailer.rb
@@ -2,7 +2,7 @@ class ElasticMailer < ActionMailer::Base
   default to: "elasticcontacts@gmail.com"
 
   def signup_notification(message)
-    mail(:subject => "Website Email", :body => "#{message.content}/n/n#{message.name} <#{message.email}>", :from => "#{message.name} <#{message.email}>")
+    mail(:subject => "Website Email", :body => "#{message.content}  #{message.name} <#{message.email}>", :from => "#{message.name} <#{message.email}>")
   end
 
 end
