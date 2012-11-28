2638d9b2e703fd7a8acf6b6b04eac80ee8e0fb1d
Andrew Coates <acoates@infotech.com>
2012-11-28 12:37:35 -0500

    precompiling

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ require "sprockets/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  # Bundler.require(*Rails.groups(:assets => %w(development test)))
+   Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
@@ -58,6 +58,8 @@ module Elastic
 
     # Enable the asset pipeline
     config.assets.enabled = true
+
+    config.assets.initialize_on_precompile = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
