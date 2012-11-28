2f0999b7c88c196db953d845423a217125ba875f
Andrew Coates <acoates@infotech.com>
2012-11-28 08:40:49 -0500

    testing staging mailer

--- app/mailers/elastic_mailer.rb
+++ app/mailers/elastic_mailer.rb
@@ -1,5 +1,6 @@
 class ElasticMailer < ActionMailer::Base
-  default to: "elasticcontacts@gmail.com"
+  default to: ENV['CONTACT_EMAIL']
+  
 
   def signup_notification(message)
     mail(:subject => "Website Email", :body => "#{message.content}  Name:#{message.name} Email:#{message.email} Phone:#{message.phone}", :from => "#{message.name} <#{message.email}>")
